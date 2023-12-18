1e4fe985752dcabecef3e840a2018fc45a539985
adamhemm <adamhemmelgarn1995@yahoo.com>
2023-12-18 15:48:55 -0500

    Added a JLabel adn set up an action listener to add the user input to a display list

--- src/smallgoalchooser/graphicinterface/ChooserFrame.java
+++ src/smallgoalchooser/graphicinterface/ChooserFrame.java
@@ -8,14 +8,15 @@ public class ChooserFrame implements ActionListener {
 	
 	private JFrame frame;
 	private JButton addButton, saveButton, yesButton, noButton;
-	private JTextField textField;
+	//private JTextField textField;
 	private JTextArea inputTextArea;
 	private JScrollPane inputScrollPane;
 	private JTextArea listDisplayArea;
 	private JScrollPane listDisplayPane;
-	private JPanel panel;
+	private JLabel helperLabel;
+	//private JPanel panel;
 	
-	private Font myFont = new Font("Ink Free", Font.BOLD, 10);
+	//private Font myFont = new Font("Ink Free", Font.BOLD, 10);
 	
 	public ChooserFrame() {
 		frame = new JFrame("Small Goal Helper");
@@ -29,33 +30,49 @@ public class ChooserFrame implements ActionListener {
 		textField.setToolTipText("Enter a small goal here");*/
 		
 		inputTextArea = new JTextArea("", 5, 50);
-		inputTextArea.setBounds(50,300,300,50);
+		inputTextArea.setBounds(50,250,300,100);
 		inputTextArea.setLineWrap(true);
 		//textArea.setEditable(false);
 		//textArea.setText("hello");
 		inputScrollPane = new JScrollPane(inputTextArea);
-		inputScrollPane.setBounds(50,300,300,50);
+		inputScrollPane.setBounds(50,250,300,100);
 		inputScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
+		listDisplayArea = new JTextArea("", 5, 50);
+		listDisplayArea.setBounds(50,100,300,100);
+		listDisplayArea.setLineWrap(true);
+		listDisplayArea.setEditable(false);
+		listDisplayPane = new JScrollPane(listDisplayArea);
+		listDisplayPane.setBounds(50, 100, 300, 100);
+		listDisplayPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		helperLabel = new JLabel("Enter a small goal and press Add to List");
+		helperLabel.setBounds(50, 50, 300, 50);
 		/*panel = new JPanel();
 		panel.setBounds(50, 10, 30, 50);
 		panel.setBackground(Color.black);
 		panel.setOpaque(true);
 		panel.setVisible(true);*/
 		
-		addButton = new JButton("Add to list");
+		addButton = new JButton("Add to List");
 		addButton.setBounds(50, 400, 100, 50);
-		saveButton = new JButton("Save list");
+		addButton.addActionListener(this);
+		saveButton = new JButton("Save List");
 		saveButton.setBounds(250, 400, 100, 50);
+		saveButton.addActionListener(this);
 		yesButton = new JButton("Yes");
 		yesButton.setBounds(50, 400, 100, 50);
+		yesButton.addActionListener(this);
 		yesButton.setVisible(false);
 		noButton = new JButton("No");
 		noButton.setBounds(250, 400, 100, 50);
+		noButton.addActionListener(this);
 		noButton.setVisible(false);
 		
 		//frame.add(textField);
 		//frame.add(textArea);
+		frame.add(helperLabel);
+		frame.add(listDisplayPane);
 		frame.add(inputScrollPane);
 		frame.add(addButton);
 		frame.add(saveButton);
@@ -67,8 +84,28 @@ public class ChooserFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getSource() == addButton) {
+			if(inputTextArea.getText().trim().length() > 0) {
+				if(listDisplayArea.getText().length() > 0) {
+					String tempTextHolder;
+					tempTextHolder = listDisplayArea.getText() + ",\n" + inputTextArea.getText();
+					listDisplayArea.setText(tempTextHolder);
+				}
+				else {
+					listDisplayArea.setText(inputTextArea.getText());
+				}
+			}
+			inputTextArea.setText(null);
+		}
+		else if(e.getSource() == saveButton) {
+			
+		}
+		else if(e.getSource() == yesButton) {
+			
+		}
+		else if(e.getSource() == noButton) {
+			
+		}
 	}
 
 }
