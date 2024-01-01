cfc3ce680cf0b4556e0ff223c83dee2a7ca7425f
adamhemm <adamhemmelgarn1995@yahoo.com>
2024-01-01 16:50:13 -0500

    Fleshed out functions in ListHandler to create a string to be saved and to handle a saved string so it can be used

--- src/smallgoalchooser/listhandler/ListHandler.java
+++ src/smallgoalchooser/listhandler/ListHandler.java
@@ -5,8 +5,10 @@ import java.util.Random;
 
 public class ListHandler {
 	private String[] goalsToDo;
+	private String[] goalsDone;
+	private String[] splitSaveFile;
 	private ArrayList<String> modifiableGoalsToDo = new ArrayList<String>();
-	private ArrayList<String> modifiableGoalsDone;
+	private ArrayList<String> modifiableGoalsDone = new ArrayList<String>();
 	private String currentGoal;
 	private int currentGoalIndex;
 	private String goalsToBeSaved;
@@ -24,7 +26,20 @@ public class ListHandler {
 	}
 	
 	public void loadFromSaveFile(String saveFile) {
-		
+		splitSaveFile = saveFile.split("=");
+		currentGoal = splitSaveFile[0];
+		if(splitSaveFile[1].trim().length() > 0) {
+			goalsToDo = splitSaveFile[1].split("+");
+			for (String goal: goalsToDo) {
+				modifiableGoalsToDo.add(goal);
+			}
+		}
+		if(splitSaveFile[2].trim().length() > 0) {
+			goalsDone = splitSaveFile[2].split("+");
+			for (String goal: goalsDone) {
+				modifiableGoalsDone.add(goal);
+			}
+		}
 	}
 	
 	public String chooseCurrentGoal() {
@@ -34,10 +49,37 @@ public class ListHandler {
 		return currentGoal;
 	}
 	
+	public void currentGoalCompleted() {
+		modifiableGoalsDone.add(currentGoal);
+	}
+	
 	public String createListToBeSaved() {
 		/*creates a formatted list to be saved,
 		 *  consisting of goals done, current goal,
 		 *  and goals to do*/
+		goalsToBeSaved = currentGoal;
+		goalsToBeSaved = goalsToBeSaved + " = ";
+		if(!modifiableGoalsToDo.isEmpty()) {
+			for(int i = 0; i<modifiableGoalsToDo.size(); i++) {
+				if((i+1)== modifiableGoalsToDo.size()) {
+					goalsToBeSaved = goalsToBeSaved + modifiableGoalsToDo.get(i);
+				}
+				else {
+					goalsToBeSaved = goalsToBeSaved + modifiableGoalsToDo.get(i) + "+";
+				}
+			}
+		}
+		goalsToBeSaved = goalsToBeSaved + " = ";
+		if(!modifiableGoalsDone.isEmpty()) {
+			for(int i = 0; i<modifiableGoalsDone.size(); i++) {
+				if((i+1)== modifiableGoalsDone.size()) {
+					goalsToBeSaved = goalsToBeSaved + modifiableGoalsDone.get(i);
+				}
+				else {
+					goalsToBeSaved = goalsToBeSaved + modifiableGoalsDone.get(i) + "+";
+				}
+			}
+		}
 		return goalsToBeSaved;
 	}
 
